d749d4d454ecb9e47d01ede2a232c3493374d25d
imloreno <work@imloreno.com>
2024-10-22 17:38:35 -0400

    TAI-3 - Adding dev script

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import manifest from './src/assets/manifest.json';
 import { crx } from '@crxjs/vite-plugin';
-import path from 'path';
+import manifest from './manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'src/main.tsx'),
-      },
-    },
-  },
 });
