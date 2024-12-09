a08fe50d4cc13a37caa553d93ec64a6152506ce0
imloreno <work@imloreno.com>
2024-12-09 16:03:39 -0400

    TAI-46 Persistent routes

--- src/Apps/PopUp/containers/Menu/index.tsx
+++ src/Apps/PopUp/containers/Menu/index.tsx
@@ -3,11 +3,32 @@ import { Box } from '../../components';
 import useMenu from './hooks/useMenu';
 import styles from './menu.module.css';
 import { useSessionStore } from '@popup:store';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const Menu = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
   const { picked, width, left } = useMenu();
   const { token } = useSessionStore();
   const routes = token ? authenticatedRoutes : noLoginRoutes;
+
+  // Redirect to the last visited page
+  useEffect(() => {
+    setTimeout(() => {
+      try {
+        localStorage.setItem('current-path', pathname);
+      } catch (e) {
+        console.error(e);
+      }
+    }, 200);
+  }, [pathname]);
+  useEffect(() => {
+    const lastPath = localStorage.getItem('current-path');
+    if (lastPath && lastPath !== pathname) {
+      navigate(lastPath);
+    }
+  }, []);
 
   return (
     <Box className={styles.menuList} id="menuContainer" containerMode>
