4021907aff3e7941536b8b8dc83661259d9b7ffb
imloreno <santyago.arias.sav@gmail.com>
2026-09-16 02:19:22 -0400

    Injector

---
+++ apps/extension/src/modules/common/components/ButtonIcon/index.tsx
@@ -0,0 +1,28 @@
+import type { ButtonHTMLAttributes, ReactNode } from 'react';
+import type { CustomizableComponent } from '@common/models';
+
+export interface ButtonIconProps
+  extends
+    CustomizableComponent,
+    Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'children'> {
+  children: ReactNode;
+}
+
+const ButtonIcon = ({
+  children,
+  type = 'button',
+  className = '',
+  style,
+  ...rest
+}: ButtonIconProps) => (
+  <button
+    type={type}
+    style={style}
+    className={`tai:flex tai:cursor-pointer tai:items-center tai:justify-center tai:rounded-full tai:disabled:cursor-not-allowed ${className}`}
+    {...rest}
+  >
+    {children}
+  </button>
+);
+
+export default ButtonIcon;
