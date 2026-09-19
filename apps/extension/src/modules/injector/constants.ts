f11d61a45dd33a919be3067d2b36d278b3569ae7
imloreno <santyago.arias.sav@gmail.com>
2026-09-19 00:23:53 -0400

    Floating buttons feaure

--- apps/extension/src/modules/injector/constants.ts
+++ apps/extension/src/modules/injector/constants.ts
@@ -2,7 +2,7 @@ export const APP_PAGE = 'index.html';
 
 export const APP_URL = chrome.runtime.getURL(APP_PAGE);
 
-export const EDGE_MARGIN = 16;
+export const EDGE_MARGIN = 5;
 export const LAUNCHER_SIZE = 56;
 export const PANEL_WIDTH = 400;
 export const PANEL_HEIGHT = 600;
